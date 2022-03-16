f653ebf15dc3ed5521f8144aa35829961d373818
Lucas Saber <L.Saber36@gmail.com>
2022-03-15 22:39:00 -0400

    added constant file with pin definitions

---
+++ constant.py
@@ -0,0 +1,35 @@
+# NOTE: The following numbers refer to GPIO numbers, not device pin numbers
+# =========================================================================
+# Bump switch definitions (1 starting from the left-most switch)
+BUTTON1 = 2
+BUTTON2 = 3
+BUTTON3 = 4
+BUTTON4 = 17
+BUTTON5 = 27
+BUTTON6 = 22
+
+# Line sensor definitions (1 starting from the left-most sensor)
+LINE_SENSOR_1 = 10
+LINE_SENSOR_2 = 9
+LINE_SENSOR_3 = 11
+LINE_SENSOR_4 = 5
+LINE_SENSOR_5 = 6
+LINE_SENSOR_6 = 13
+LINE_SENSOR_7 = 19
+LINE_SENSOR_8 = 26
+
+# Motor pin definitions
+LEFT_MOTOR_PWM = 12
+LEFT_MOTOR_DIR = 7
+LEFT_MOTOR_SLP = 8
+
+RIGHT_MOTOR_PWM = 18 
+RIGHT_MOTOR_DIR = 23
+RIGHT_MOTOR_SLP = 24
+
+# Encoder definitions
+LEFT_ENCODER_B = 25
+LEFT_ENCODER_A = 16
+
+RIGHT_ENCODER_A = 20
+RIGHT_ENCODER_B = 21
