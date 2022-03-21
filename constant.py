353e2805d42ad586642d60a96203ab212ae46c13
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-03-21 15:06:29 -0400

    Put pin definitions into a dictionary

--- constant.py
+++ constant.py
@@ -2,34 +2,47 @@
 # =========================================================================
 # Bump switch definitions (1 starting from the left-most switch)
 # These button pins need to have a pull-up resistor enabled
-BUTTON1 = 2
-BUTTON2 = 3
-BUTTON3 = 4
-BUTTON4 = 17
-BUTTON5 = 27
-BUTTON6 = 22
 
-# Line sensor definitions (1 starting from the left-most sensor)
-LINE_SENSOR_LED = 24
-LINE_SENSOR_1 = 25
-LINE_SENSOR_2 = 8
-LINE_SENSOR_4 = 7
-LINE_SENSOR_5 = 12
-LINE_SENSOR_7 = 16
-LINE_SENSOR_8 = 20
+CONSTANTS = {
 
-# Motor pin definitions
-LEFT_MOTOR_SLP = 10
-LEFT_MOTOR_DIR = 9
-LEFT_MOTOR_PWM = 11
+    # Bump Sensor Definitions
+    "BUTTON1":2,
+    "BUTTON2":3,
+    "BUTTON3":4,
+    "BUTTON4":17,
+    "BUTTON5":27,
+    "BUTTON6":22,
 
-RIGHT_MOTOR_SLP = 5
-RIGHT_MOTOR_DIR = 6
-RIGHT_MOTOR_PWM = 13 
+    # Line sensor definitions (1 starting from the left-most sensor)
+    "LINE_SENSOR_LED":24,
+    "LINE_SENSOR_1":25,
+    "LINE_SENSOR_2":8,
+    "LINE_SENSOR_4":7,
+    "LINE_SENSOR_5":12,
+    "LINE_SENSOR_7":16,
+    "LINE_SENSOR_8":20,
 
-# Encoder definitions
-LEFT_ENCODER_B = 23
-LEFT_ENCODER_A = 18
+    # Motor pin definitions
+    "LEFT_MOTOR_SLP":10,
+    "LEFT_MOTOR_DIR":9,
+    "LEFT_MOTOR_PWM":11,
 
-RIGHT_ENCODER_A = 19
-RIGHT_ENCODER_B = 26
+    "RIGHT_MOTOR_SLP":5,
+    "RIGHT_MOTOR_DIR":6,
+    "RIGHT_MOTOR_PWM":13 ,
+
+    # Encoder definitions
+    "LEFT_ENCODER_B": 23,
+    "LEFT_ENCODER_A": 18,
+
+    "RIGHT_ENCODER_A":19,
+    "RIGHT_ENCODER_B":26
+
+}
+
+
+
+
+
+
+
