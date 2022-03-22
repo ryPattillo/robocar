612ba9a441382348912c73af34f821c51d8c97be
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-03-22 00:17:16 -0400

    reading sensor data

--- move-forward-test.py
+++ move-forward-test.py
@@ -6,11 +6,8 @@ from constant import CONSTANTS as C
 
 # Set mode
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(C["BUTTON1"], GPIO.IN)
-GPIO.setup(C["BUTTON2"], GPIO.IN)
 GPIO.setup(C["BUTTON4"], GPIO.IN)
-
 
 GPIO.setup(C["LEFT_MOTOR_SLP"], GPIO.OUT)
 GPIO.setup(C["LEFT_MOTOR_DIR"], GPIO.OUT)
@@ -21,8 +18,8 @@ GPIO.setup(C["RIGHT_MOTOR_DIR"], GPIO.OUT)
 GPIO.setup(C["RIGHT_MOTOR_PWM"], GPIO.OUT)
 
 
+# set up line sensors
 GPIO.setup(C["LINE_SENSOR_LED"], GPIO.OUT)
-
 GPIO.setup(C["LINE_SENSOR_1"], GPIO.IN)
 GPIO.setup(C["LINE_SENSOR_2"], GPIO.IN)
 GPIO.setup(C["LINE_SENSOR_4"], GPIO.IN)
@@ -30,9 +27,7 @@ GPIO.setup(C["LINE_SENSOR_5"], GPIO.IN)
 GPIO.setup(C["LINE_SENSOR_7"], GPIO.IN)
 GPIO.setup(C["LINE_SENSOR_8"], GPIO.IN)
 
-
 GPIO.output(C["LEFT_MOTOR_SLP"], 0)
-
 
 # control left motor
 GPIO.output(C["LEFT_MOTOR_SLP"], 1)
@@ -52,33 +47,33 @@ try:
 
   while True:
 
-    GPIO.output(C["LINE_SENSOR_LED"], 1)
-    sleep(0.1) #wait for the cap to charge. Equations available on wikipedia for working out exact times
-    r2 = GPIO.input(C["LINE_SENSOR_1"])
-    r3 = GPIO.input(C["LINE_SENSOR_2"])
-    r4 = GPIO.input(C["LINE_SENSOR_4"])
-    r5 = GPIO.input(C["LINE_SENSOR_5"])
-    r6 = GPIO.input(C["LINE_SENSOR_7"])
-    r7 = GPIO.input(C["LINE_SENSOR_8"])
-    GPIO.output(C["LINE_SENSOR_LED"], 0)
-    print(r2,r3,r4,r5,r6,r7)
+    # GPIO.output(C["LINE_SENSOR_LED"], 1)
+    # sleep(0.1) #wait for the cap to charge. Equations available on wikipedia for working out exact times
+    # r2 = GPIO.input(C["LINE_SENSOR_1"])
+    # r3 = GPIO.input(C["LINE_SENSOR_2"])
+    # r4 = GPIO.input(C["LINE_SENSOR_4"])
+    # r5 = GPIO.input(C["LINE_SENSOR_5"])
+    # r6 = GPIO.input(C["LINE_SENSOR_7"])
+    # r7 = GPIO.input(C["LINE_SENSOR_8"])
+    # GPIO.output(C["LINE_SENSOR_LED"], 0)
+    # print(r2,r3,r4,r5,r6,r7)
    
 
-    # # BUTTON1 starts the motors
-    # if (not GPIO.input(C["BUTTON1"])):
-    #   left.start(50)
-    #   right.start(50)
+    # BUTTON1 starts the motors
+    if (not GPIO.input(C["BUTTON1"])):
+      left.start(50)
+      right.start(50)
 
-    #  # BUTTON3 stops the motors 
-    # elif (not GPIO.input(C["BUTTON2"])):
-    #   left.stop()
-    #   right.stop()
+     # BUTTON3 stops the motors 
+    elif (not GPIO.input(C["BUTTON2"])):
+      left.stop()
+      right.stop()
 
-     # stop execution for testing
-    if (not GPIO.input(C["BUTTON2"])):
-      print("Tset")
-      GPIO.cleanup()
-      exit()
+    #  # stop execution for testing
+    # if (not GPIO.input(C["BUTTON2"])):
+    #   print("Tset")
+    #   GPIO.cleanup()
+    #   exit()
 
   
 finally:
