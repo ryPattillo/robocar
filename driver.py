d74a4c22c7c083022716baaa46298272f761e321
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-03-24 15:50:25 -0400

    Put setup function back in

--- driver.py
+++ driver.py
@@ -5,49 +5,7 @@ from constant import CONSTANTS as C
 from pin_setup import setup
 from picamera import PiCamera
 
-
-# Set mode
-GPIO.setmode(GPIO.BCM)
-
-# Configure Bumb Sensors
-GPIO.setup(C["BUTTON1"], GPIO.IN)
-GPIO.setup(C["BUTTON2"], GPIO.IN)
-
-# NOTE: Pull up resistors: put the power between +V and signal
-# NOTE: Pull down resistors: Put in between +v and ground (-v)  
-GPIO.setup(C["BUTTON3"], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(C["BUTTON4"], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(C["BUTTON5"], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(C["BUTTON6"], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-# Configure left motor
-GPIO.setup(C["LEFT_MOTOR_SLP"], GPIO.OUT)
-GPIO.setup(C["LEFT_MOTOR_DIR"], GPIO.OUT)
-GPIO.setup(C["LEFT_MOTOR_PWM"], GPIO.OUT)
-
-# Configure right motor
-GPIO.setup(C["RIGHT_MOTOR_SLP"], GPIO.OUT)
-GPIO.setup(C["RIGHT_MOTOR_DIR"], GPIO.OUT)
-GPIO.setup(C["RIGHT_MOTOR_PWM"], GPIO.OUT)
-
-# The led needed for the light sensors to work
-GPIO.setup(C["LINE_SENSOR_LED"], GPIO.OUT)
-
-# Allow for input from each of the sensors
-GPIO.setup(C["LINE_SENSOR_1"], GPIO.IN,pull_up_down = GPIO.PUD_UP)
-GPIO.setup(C["LINE_SENSOR_2"], GPIO.IN,pull_up_down = GPIO.PUD_UP)
-GPIO.setup(C["LINE_SENSOR_4"], GPIO.IN,pull_up_down = GPIO.PUD_UP)
-GPIO.setup(C["LINE_SENSOR_5"], GPIO.IN,pull_up_down = GPIO.PUD_UP)
-GPIO.setup(C["LINE_SENSOR_7"], GPIO.IN,pull_up_down = GPIO.PUD_UP)
-GPIO.setup(C["LINE_SENSOR_8"], GPIO.IN,pull_up_down = GPIO.PUD_UP)
-
-    # Set up left motor contorls
-GPIO.output(C["LEFT_MOTOR_SLP"], 1)
-GPIO.output(C["LEFT_MOTOR_DIR"], 0)
-# Set up right motor controls
-GPIO.output(C["RIGHT_MOTOR_SLP"], 1)
-GPIO.output(C["RIGHT_MOTOR_DIR"], 0)
-
+setup()
 left =  GPIO.PWM(C["LEFT_MOTOR_PWM"], 1000)
 right = GPIO.PWM(C["RIGHT_MOTOR_PWM"], 1000)
 
