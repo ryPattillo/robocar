e789be86a105ea002ba54de61689750397c60802
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-03-24 20:38:48 +0000

    TEsting line sensors

--- driver.py
+++ driver.py
@@ -5,6 +5,8 @@ from constant import CONSTANTS as C
 from pin_setup import setup
 from picamera import PiCamera
 
+GPIO.cleanup()
+
 setup()
 left =  GPIO.PWM(C["LEFT_MOTOR_PWM"], 1000)
 right = GPIO.PWM(C["RIGHT_MOTOR_PWM"], 1000)
@@ -44,13 +46,22 @@ def end(channel):
     GPIO.cleanup()
     exit()
 
-def line_found(channel):
+def line_rising(channel):
     '''
     Called when a line is detected from the sensor
     '''
-    print("LINE FOUND")    
+    print("LINE RISING")    
+
+def line_falling(channel):
+
+    '''
+    Called when a line is detected from the sensor
+    '''
+    print("LINE FALLING")    
 
 if __name__ == "__main__":
+
+
 
   # set up all the pin
 
@@ -64,14 +75,20 @@ if __name__ == "__main__":
   GPIO.PWM(C["LEFT_MOTOR_PWM"], 1000).stop()
   GPIO.PWM(C["RIGHT_MOTOR_PWM"], 1000).stop()
 
+
+  # Make I/O line an input
+
   print("BUTTON 1 STARTS MOTORS")
   print("BUTTON 2 STOPS MOTORS")
   print("BUTTON 3 ENDS EXECUTION")
 
   # NOTE: we need to get line sensors working with the event detectors 
-  #GPIO.add_event_detect(C["LINE_SENSOR_1"],GPIO.RISING, callback = line_found,bouncetime = 500)
-  #GPIO.add_event_detect(C["LINE_SENSOR_4"],GPIO.RISING, callback = line_found,bouncetime = 500)
+  #GPIO.add_event_detect(C["LINE_SENSOR_1"],GPIO.FALLING, callback = line_falling,bouncetime = 1)
+  #GPIO.add_event_detect(C["LINE_SENSOR_4"],GPIO.FALLING, callback = line_falling,bouncetime =1)
 
+
+  GPIO.add_event_detect(C["LINE_SENSOR_1"],GPIO.RISING, callback = line_rising,bouncetime = 1)
+  GPIO.add_event_detect(C["LINE_SENSOR_4"],GPIO.RISING, callback = line_rising,bouncetime = 1)
 
   # Events for when the buttons are clicked
   # NOTE: All of these are being called correctly however the motors are not being start up
@@ -85,11 +102,20 @@ if __name__ == "__main__":
 
   while True:
 
-    pass
+    # Drive output high
+    GPIO.output(C["LINE_SENSOR_LED"],1)
+    # Allow at least 10 micro seconds
+    sleep(0.00001)
+    GPIO.output(C["LINE_SENSOR_LED"],0)
+
+
+
+
     # Capture an image every 2 seconds
-    # sleep(2)
-    #camera.capture('foo.jpg')
+    sleep(2)
+    camera.capture('foo.jpg')
   
+
     # find_sign('image.jp')
     # if sign == 'speed_up':
         # speed up
