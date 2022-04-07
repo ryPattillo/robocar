159e23667513dd74e64cb3a269a4d1c449a65d64
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-04-07 11:34:41 -0400

    Setting up more teleop

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pin_setup import setup
 # Configure all the pins
 setup()
 if __name__ == "__main__":
-    drive_mode = int(input("[0] for teleop, [1] for freeroam: "))
-    if drive_mode == 1:
+    drive_mode = int(input("[0] for freeroam, [1] for teleop: "))
+    if drive_mode == 0:
         cruising_speed = float(input("enter default cruising speed: "))
     driver = Driver(drive_mode,cruising_speed)
     driver.drive()
